7af307c81948596f90c4823292a98faa4057c31d
ha282 <habdi27a@gmail.com>
2025-08-13 12:15:54 +0100

    EDA for city csv

--- src/extract/extract_city.py
+++ src/extract/extract_city.py
@@ -53,6 +53,7 @@ def extract_city_execution() -> pd.DataFrame:
 
     # Execute the query
     city_df = execute_extract_query(query, connection)
+    city_df.to_csv("data/raw/unclean-city.csv", index=False)
     connection.close()
 
     # Return the created DataFrame
