500f4f0d374ca10c27dad8c3ab9e49f55e435d4c
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-08-13 14:21:47 +0100

    transformed customers table

---
+++ src/transform/clean_customers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def clean_customers(customers: pd.DataFrame) -> pd.DataFrame:
+    # Handle Missing Values
+    customers = remove_missing_values(customers)
+
+    # Save the dataframe as a CSV for logging purposes
+    # Ensure the directory exists
+    customers.to_csv("data/processed/cleaned-customers.csv", index=False)
+    customers.info()
+    return customers
+
+
+def remove_missing_values(customers: pd.DataFrame) -> pd.DataFrame:
+    # Remove unused columns and columns with missing values
+    customers = customers.dropna(subset="address_id")
+
+    return customers
--- src/transform/transform.py
+++ src/transform/transform.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from typing import Tuple
 from src.transform.clean_address import clean_address
+from src.transform.clean_customers import clean_customers
 
 
 def transform_data(data) -> Tuple[pd.DataFrame, pd.DataFrame]:
     cleaned_address = clean_address(data[5])
+    cleaned_customers = clean_customers(data[2])
 
-    return (cleaned_address)
+    return (cleaned_address, cleaned_customers)
